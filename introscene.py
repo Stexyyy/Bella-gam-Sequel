cbbe9b815350526c79c8553db231745146f0e494
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2023-05-10 09:29:11 -0600

    Update introscene.py

--- introscene.py
+++ introscene.py
@@ -7,10 +7,13 @@ screen = pygame.display.set_mode((800, 800))
 
 bananastare = pygame.image.load ("silly1.jpg")
 brokesad = pygame.image.load("sillyeyesclosed.jpg")
-#schmoney = pygame.image.load("images/Schmoney.jpg")
-#cheeser = beller.get_rect(topleft = (200,280))
-#moner = schmoney.get_rect(topleft= (100,280))
-#bruher = schmoney.get_rect(topleft= (100,280))
+sandwich = pygame.image.load("sandwich.jpg")
+bella = pygame.image.load("AFKBella.jpg")
+cheese = bananastare.get_rect(topleft = (200,280))
+food = sandwich.get_rect(topleft= (100,280))
+bruh = brokesad.get_rect(topleft= (500,400))
+mom = bella.get_rect(topleft= (500, 400))
+
 
 def backstory():
 
@@ -18,8 +21,31 @@ def backstory():
 
   for x in range(255):
       screen.fill((255, 255, 255))
-      text = font.render('This dog has sad past...', True, (255-x,255-x,255-x), (255,255,255))
-      screen.blit(bananastare, cheeser)
+      text = font.render('This dog has sad past... (trust)', True, (255-x,255-x,255-x), (255,255,255))
+      screen.blit(bananastare, cheese)
       screen.blit(text, (0,100))
+      pygame.display.flip()
+      time.sleep(20 / 1000)
+      
+  for x in range(255):
+      screen.fill((255, 255, 255))
+      text = font.render('This dog was related to the one who has riches and fame', True, (0,0,0), (255,255,255))
+      text2 = font.render('But unfortuntely, the one with riches became corrupted', True, (255-x,255-x,255-x), (255,255,255))
+      screen.blit(bananastare, cheese)
+      screen.blit(bella, mom)
+      screen.blit(text, (0,100))
+      screen.blit(text2, (0,200))
+      pygame.display.flip()
+      time.sleep(20 / 1000)
+      
+      
+  for x in range(255):
+      screen.fill((255, 255, 255))
+      text = font.render('Because of this.. she stole his sandwhich.', True, (0,0,0), (255,255,255))
+      text2 = font.render('and he was sad...', True, (255-x,255-x,255-x), (255,255,255))
+      screen.blit(brokesad, bruh)
+      screen.blit(sandwich, food)
+      screen.blit(text, (0,100))
+      screen.blit(text2, (0,200))
       pygame.display.flip()
       time.sleep(20 / 1000)
